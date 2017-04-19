3cd49bb79c9a91d652b9ffc0b307ccdf1bffdfc8
TroyHou <1009372153@qq.com>
2017-04-19 17:47:03 +0800

    style table

--- resources/views/wechat/form/parent.blade.php
+++ resources/views/wechat/form/parent.blade.php
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 <div class="box">
     <h3 class="Info patriarch">家长信息</h3>
     <div class="weui-cell cells">
         <div class="weui-cell__hd">
             <label class="weui-label">家长姓名<span> *</span></label>
         </div>
-=======
-<h3 class="Info">家长信息</h3>
-<div class="weui-cells__title">家长姓名<span> *</span></div>
-<div class="weui-cells">
-    <div class="weui-cell">
->>>>>>> 075a773af3e18004567b26c6b84cdeeda6e78c01
         <div class="weui-cell__bd">
             <input class="weui-input" type="text"  placeholder="家长姓名" value="{{ old('inputParent') }}" name="inputParent">
         </div>
@@ -39,7 +32,6 @@
             <input class="weui-input" type="text" placeholder="联系邮箱" value="" name="inputEmail">
         </div>
     </div>
-<<<<<<< HEAD
     <div class="weui-cell cells">
         <div class="weui-cell__hd">
             <label class="weui-label">家长特殊嘱咐</label>
@@ -53,14 +45,3 @@
          <a href="javascript:;" class="weui-btn weui-btn_primary">下一步</a>
     </div>
 </div>
-=======
-</div>
-{{--<div class="weui-cells__title">微信ID（开营前5天建立微信群）</div>--}}
-{{--<div class="weui-cells">--}}
-{{--<div class="weui-cell">--}}
-{{--<div class="weui-cell__bd">--}}
-{{--<input class="weui-input" type="text" placeholder="微信ID" value="" name="inputWechat">--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
->>>>>>> 075a773af3e18004567b26c6b84cdeeda6e78c01
